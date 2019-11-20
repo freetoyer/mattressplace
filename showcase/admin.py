53dae3ba6117114a72f7bb77c98b42e29e31d766
freetoyer <andrey_89-89@mail.ru>
2019-11-20 23:10:25 +0300

    add models Product, Product_Image

--- showcase/admin.py
+++ showcase/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
-from showcase.models import Category
+from showcase.models import Category, Product, Product_Image, Product_Size, Size_Price
+
+class Product_Image_Admin(admin.ModelAdmin):
+    readonly_fields = ('slug', 'name',)
 
 admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(Product_Image, Product_Image_Admin)
+admin.site.register(Product_Size)
+admin.site.register(Size_Price)
--- showcase/models.py
+++ showcase/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 from django.urls import reverse
+from decimal import Decimal
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from django.utils.text import slugify
 
 
 def image_folder(instance, filename):
@@ -18,3 +22,45 @@ class Category(models.Model):
     def get_absolute_url(self):
         return reverse('category_detail', kwargs={'category_slug': self.slug})
 
+
+class Product(models.Model):
+
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Product_Image(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='product_images')
+    image = models.ImageField(upload_to=image_folder)
+    name = models.CharField(max_length=50)
+    slug = models.SlugField()
+
+    def __str__(self):
+        return self.name
+
+@receiver(pre_save, sender=Product_Image)
+def pre_save_product_image_slug(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        pre_save_product_image_slug.counter += 1
+        slug = slugify(instance.product.title) + '_' + str(pre_save_product_image_slug.counter)
+        name = slug.upper()
+        instance.slug = slug
+        instance.name = name
+
+pre_save_product_image_slug.counter = 0
+
+
+class Product_Size(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='product_sizes')
+    size = models.CharField(max_length=10)
+
+
+class Size_Price(models.Model):
+    size = models.OneToOneField(Product_Size, on_delete=models.CASCADE, related_name='price_of_size')
+    price = models.DecimalField(max_digits=9, decimal_places=2)
+    
