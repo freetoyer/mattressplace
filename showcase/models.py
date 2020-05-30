615d6a4323854eded8a759f2a80d916cfd5ca7ab
freetoyer <andrey_89-89@mail.ru>
2020-05-30 12:13:08 +0300

    1. save() corrected 2. replaced .format to f-string

--- showcase/models.py
+++ showcase/models.py
@@ -1,37 +1,37 @@
 from django.db import models
 from django.urls import reverse
 from decimal import Decimal
-from django.db.models.signals import pre_save
-from django.dispatch import receiver
+# from django.db.models.signals import pre_save
+# from django.dispatch import receiver
 from django.utils.text import slugify
-
+ 
 from ckeditor.fields import RichTextField
 
 
 def image_folder(instance, filename):
-    filename = instance.slug + '.' + filename.split('.')[1]
+    filename = f"{instance.slug}.{filename.split('.')[1]}"
     general_folder = instance.__class__.__name__
     if general_folder=='Product_Image':
         foldername = slugify(instance.product.title).capitalize()
     else:
         foldername = instance.slug
-    return '{0}/{1}/{2}'.format(general_folder, foldername, filename)
+    return f"{general_folder}/{foldername}/{filename}"
 
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
     slug = models.SlugField()
     image = models.ImageField(upload_to=image_folder)
-
+ 
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        
     def __str__(self):
         return self.name
 
     def get_absolute_url(self):
         return reverse('category_detail', kwargs={'category_slug': self.slug})
- 
-    class Meta:
-        verbose_name = 'Категория'
-        verbose_name_plural = 'Категории'
 
 
 class Product(models.Model):
@@ -41,18 +41,18 @@ class Product(models.Model):
     description = RichTextField()
     slug = models.SlugField()
 
-    def __str__(self):
-        return self.title
-
     class Meta:
         verbose_name = 'Продукт'
         verbose_name_plural = 'Продукты'
+
+    def __str__(self):
+        return self.title
 
     def save(self, *args, **kwargs):
         if not self.slug:
             slug = slugify(self.title)
             self.slug = slug
-            super().save(*args, **kwargs)
+        super().save(*args, **kwargs)
 
 
 class Product_Image(models.Model):
@@ -61,13 +61,13 @@ class Product_Image(models.Model):
     name = models.CharField(max_length=50)
     slug = models.SlugField()
     main_image = models.BooleanField(default=False)
-
-    def __str__(self):
-        return self.name
      
     class Meta:
         verbose_name = 'Фото продукта'
         verbose_name_plural = 'Фото продуктов'
+
+    def __str__(self):
+        return self.name
 
     def save(self, *args, **kwargs):
         if not self.slug:
@@ -75,10 +75,10 @@ class Product_Image(models.Model):
                 image_number = self.product.product_images.count() + 1
             else:
                 image_number = 1
-            slug = self.product.slug + '_' + str(image_number)
+            slug = f"{self.product.slug}_{image_number}"
             self.slug = slug
             self.name = slug.capitalize()
-            super().save(*args, **kwargs)
+        super().save(*args, **kwargs)
             
 
 class Product_Size(models.Model):
@@ -88,19 +88,19 @@ class Product_Size(models.Model):
     slug = models.SlugField()
     price = models.DecimalField(max_digits=9, decimal_places=2)
 
-    def __str__(self):
-        return self.name
-
     class Meta:
         verbose_name = 'Размер'
         verbose_name_plural = 'Размеры'
 
+    def __str__(self):
+        return self.name
+
     def save(self, *args, **kwargs):
         if not self.slug:
-            slug = self.product.slug + '_' + self.size
+            slug = f"{self.product.slug}_{self.size}"
             self.slug = slug
             self.name = slug.capitalize()
-            super().save(*args, **kwargs)
+        super().save(*args, **kwargs)
 
 
 class Certificate(models.Model):
@@ -109,33 +109,33 @@ class Certificate(models.Model):
     image = models.ImageField(upload_to=image_folder)
     description = RichTextField()
 
-    def __str__(self):
-        return self.name
-
     class Meta:
         verbose_name = 'Сертификат'
         verbose_name_plural = 'Сертификаты'
+
+    def __str__(self):
+        return self.name
         
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(self.name, allow_unicode=True)
-            super().save(*args, **kwargs)
+        super().save(*args, **kwargs)
 
 
 class CatalogueFile(models.Model):
     name = models.CharField(max_length=100)
     slug = models.SlugField()
     file = models.FileField(upload_to=image_folder)
-
+ 
+    class Meta:
+        verbose_name = 'Каталог'
+        verbose_name_plural = 'Каталоги'
+        
     def __str__(self):
         return self.name
 
 #    def get_absolute_url(self):
 #       return reverse('category_detail', kwargs={'category_slug': self.slug})
- 
-    class Meta:
-        verbose_name = 'Каталог'
-        verbose_name_plural = 'Каталоги'
 
     def get_absolute_url(self):
         url = self.file.url
